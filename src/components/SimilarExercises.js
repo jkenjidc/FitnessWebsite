df6f390d0000d01ba2d3054bcedfa3fc98c76b7b
kenji <kenjidckenji@gmail.com>
2023-11-18 13:39:10 -0500

    removed spinner loader

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -4,8 +4,6 @@ import  HorizontalScrollBar from './HorizontalScrollBar';
 import Loader from './Loader';
 
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises}) => {
-  console.log(targetMuscleExercises)
-  console.log(equipmentExercises)
   return (
     <Box sx = {{ mt: {lg: '100px', xs: '0'}}}>
       <Typography variant = "h3" mb = {5}>Exercises that target the same muscle group</Typography>
